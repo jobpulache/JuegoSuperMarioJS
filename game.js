1cbeec1b835c494e9582e9a46ad386dc653d9a45
Job <pulachecarrenojob837@gmail.com>
2024-11-21 17:33:15 -0500

    hacemos el js - config

--- game.js
+++ game.js
@@ -4,18 +4,19 @@ type: Phaser.AUTO,
 width:256,
 height:244,
 backgroundColor:'#fff',
-parent:'game',
+parent:'game',//El parent donde se va a renderizar mnuestro juego es el elemento con el identificador "game"
 scene:{
     preload, //Con esto se ejecuta para precargar los recurssos que vamos necesitar
     create, //Se ejecuta cuando el juego comienza
-    update//Se ejcuta en cada frame
+    update//Se ejcuta en cada frame - se ejecuta constantemente
 }
 }
-new Phaser.Game(config)
+new Phaser.Game(config)//Le pasamos la configuración
 
-function preload(){//This se ejecura primero
+function preload(){//This se ejecuta primero
 
 }
-function create(){}//Next
+function create(){}//then
 
-function update(){}//execute continuously(continuamente)
+function update(){}//execute continuously(continuamente)- en bucle infinito
+
