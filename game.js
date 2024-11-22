19c2aaff33ea5c61f64815ff58a33f3be9e1d48b
Job <pulachecarrenojob837@gmail.com>
2024-11-21 22:51:16 -0500

    add physics, collision, jumping and falling when there is no ground

--- game.js
+++ game.js
@@ -5,6 +5,14 @@ width:256,
 height:244,
 backgroundColor:'#049cd8',
 parent:'game',//El parent donde se va a renderizar mnuestro juego es el elemento con el identificador "game"
+physics:{//Esto se encarga de la fisica
+    default: 'arcade',
+    arcade:{
+        gravity: {y:300},//Mi eje Y(hacia abajo) tenga esta gravedad
+        debug:false
+    }
+
+},
 scene:{
     preload, //Con esto se ejecuta para precargar los recurssos que vamos necesitar
     create, //Se ejecuta cuando el juego comienza
@@ -28,6 +36,8 @@ this.load.image('floorbricks',
 )
 
 
+
+
 }
 function create(){
     //Luego de cargar la imagen, la mostramos
@@ -35,21 +45,89 @@ function create(){
     .setOrigin(0,0)
     .setScale(0.15)//Escalamos la imagen(converting in small)
     
+    //ADD para que el suelo tenga colisión y retenga al mario
+    //En las physics añadimos un grupo estatico
+    this.floor = this.physics.add.staticGroup()
+
+    this.floor
+    .create(0, config.height -16, 'floorbricks')
+    .setOrigin(0,0.5)
+    .refreshBody()
+
+    this.floor
+    .create(150, config.height -16, 'floorbricks')
+    .setOrigin(0,0.5)
+    .refreshBody()//Hacer que si hay un agujero mario caiga - pilla la posición de las physics
+
     //0-empiece abajo en la izquierda--altura - ancho -- tamaño de la altura -- de que elemento?
-    this.add.tileSprite(0, config.height, config.width, 32, 'floorbricks')
-    .setScale(1)
-    .setOrigin(0,1)
+  //This config.height sale de mi mi config. 
+ 
 
+
+    // // creamos otro suelo
+    // this.add.tileSprite(100, config.height-32, 64, 32, 'floorbricks')
+    // .setOrigin(0,0)
+
+    //Lo guardamos en un objeto
+//  this.mario=   this.add.sprite(50,210, 'mario')
+//     .setOrigin(0,1)
+
+    this.mario = this.physics.add.sprite(50,100, 'mario')
+    .setOrigin(0,1)
+    .setCollideWorldBounds(true)//Evitamos que mario salga de su mundillo
+    .setGravityY(300)//Podemos modificar la gravedad que viene por default
+
+   //agregamos colisión de mario con el suelo para que no caiga
+   this.physics.add.collider(this.mario, this.floor)
+
+    //Creamos las animaciones
+this.anims.create({
+    key: 'mario-walk',
+    //creamoa frames - Le decimos cuál es el spriteSheet que queremos utiliar
+    frames: this.anims.generateFrameNumbers('mario',//Del mario quiero que me crees una animación que vaya desde el frame de  " " a ...
+        {start: 3, end: 2}
+    ),
+     frameRate: 12,//Es la velocidad a la  que se va a realizar la animación
+    //Le decimos cuantas veces se tiene que repetir
+    repeat: -1// Infinito
+})
+//Creo otra animación para cuando mario no esta haciendo nada 
+this.anims.create({
+    key: 'mario-idle',
+    frames: [{key: 'mario',frame:0}]//Del spriteMARIO utiliza el frame 0
+})
+//Animation for jump
+this.anims.create({
+    key:'mario-jump',
+    frames: this.anims.generateFrameNumbers('mario',
+        {start: 2, end: 5}
+    )
+})
+
+    //Movimiento del mario. COn las llaves
+    this.keys = this.input.keyboard.createCursorKeys()// createCursorKeys este es ubn metodo que lo que va a ser es poder visualizar las teclas en la funcion Update  
     
-    this.add.sprite(50,210, 'mario')
-    .setOrigin(0,1)
-
-
-
-
-
-
 }//then
 
-function update(){}//execute continuously(continuamente)- en bucle infinito
+function update(){//execute continuously(continuamente)- en bucle infinito
+    //Ahora dentro de update vamos a tener acceso 
+    if(this.keys.left.isDown){//Si la tecla de la izquierda se esta presionando
+    this.mario.x-=2//Movemos el mario en eje X-2
 
+    this.mario.flipX = true//Si va a la izquierda gire la X
+    //Le pasamos la animación que tiene que ejecutar
+    this.mario.anims.play('mario-walk', true)//true-Si ya existe esta animación no se ejecuta
+    }else if(this.keys.right.isDown){//Podemos hacer lo contrario(mover derecha)
+        this.mario.anims.play('mario-walk', true)
+        this.mario.x +=2
+        this.mario.flipX=false//Deje de girar a mario
+
+    }else{
+        this.mario.anims.play('mario-idle', true)//Sino estoy pulsando ni derecha, ni izquiera. Entonces ejecuta esto(queda parado)
+    }
+    if(this.keys.up.isDown && this.mario.body.touching.down){//vemosque el mario(elemento)esta tocando por abajo
+        this.mario.setVelocityY(-300)//Velocidad del salto
+        this.mario.anims.play('mario-jump', true)//Animation jump
+    }
+
+}
